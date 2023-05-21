06979d9d261e4ba5b11d13af473ec35425d58deb
Hafsa05 <hafsa@g.bracu.ac.bd>
2023-05-21 23:31:03 +0600

    home page modified

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -10,49 +10,6 @@ import ShopCategory from './ShopCategory/ShopCategory'
 const Home = () => {
 	return (
 		<>
-			{/* <div className="absolute rounded-xl h-full left-0 right-0 flex items-center bottom-0 ">
-						<div className='text-white font-bold font-serif space-y-8 pl-14 w-1/2'>
-							<h2 className='text-5xl font-extrabold'>Disney Dreamland Doll</h2>
-							<p className='font-semibold'>Discover a world of magic and nostalgia with us. </p>
-							<div>
-								<button className="btn btn-outline mr-5">Buy Toys</button>
-								<button className="btn btn-outline">All Toys</button>
-							</div>
-						</div>
-					</div> */}
-
-			{/* <div className="carousel w-full h-[750px]">
-				<div id="item1" className="carousel-item w-full">
-					<img src={banner1} className="w-full" />
-					
-
-
-				</div>
-				<div id="item2" className="carousel-item w-full">
-					<img src={banner2} className="w-full" />
-					
-
-				</div>
-				<div id="item3" className="carousel-item w-full">
-					<img src={banner3} className="w-full" />
-				</div>
-				<div id="item4" className="carousel-item w-full">
-					<img src={banner4} className="w-full" />
-				</div>
-				<div id="item5" className="carousel-item w-full">
-					<img src={banner5} className="w-full" />
-				</div>
-
-			</div>
-			<div className="flex justify-center w-full py-2 gap-2 bg-purple-50">
-				<a href="#item1" className="btn btn-xs bg-purple-200 border-white w-10">1</a>
-				<a href="#item2" className="btn btn-xs bg-purple-200 border-white w-10">2</a>
-				<a href="#item3" className="btn btn-xs bg-purple-200 border-white w-10">3</a>
-				<a href="#item4" className="btn btn-xs bg-purple-200 border-white w-10">4</a>
-				<a href="#item5" className="btn btn-xs bg-purple-200 border-white w-10">5</a>
-
-			</div> */}
-
 			<div className="carousel w-full h-[800px] rounded-xl">
 				<div id="slide1" className="carousel-item relative w-full">
 					<img src={banner1} className="w-full " />
@@ -92,7 +49,7 @@ const Home = () => {
 						<a href="#slide3" className="btn btn-circle btn-outline btn-primary bg-purple-300">❯</a>
 					</div>
 
-					
+
 				</div>
 				<div id="slide3" className="carousel-item relative w-full">
 					<img src={banner3} className="w-full " />
